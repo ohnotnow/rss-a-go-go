73bfcff5decdc585cba18d10acbc15f50f46fd75
Billy <billybofh@gmail.com>
2019-01-26 10:53:50 +0000

    Pulled in purgecss to reduce css size

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,25 @@
 const tailwindcss = require("tailwindcss");
-
+const purgecss = require('@fullhuman/postcss-purgecss')
 const autoprefixer = require("autoprefixer");
+const postcssImport = require('postcss-import')
 
 module.exports = {
   plugins: [
+    postcssImport,
     tailwindcss("./tailwind.js"),
+    purgecss({
+      content: ['./src/**/*.vue'],
+      extractors: [
+        {
+            extractor: class TailwindExtractor {
+                static extract(content) {
+                    return content.match(/[A-z0-9-:\/]+/g) || [];
+                }
+            },
+            extensions: ['vue']
+        }
+      ]
+    }),
 
     autoprefixer({
       add: true,
